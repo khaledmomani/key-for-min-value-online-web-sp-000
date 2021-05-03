0badd62c90e483d99fd09782265ba3fe53a3cd54
Khaled S Momani <khaledmomani4@gmail.com>
2021-05-03 05:29:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  harolds_things = { "sex dungeon chairs" = 5, "weird bench that you don't want to know about" = 2, "minifridge contents, just don't ask" = 9, "Harold knows what this one is" = 32}
+  harolds_things = { "sex dungeon chairs" => 5, "weird bench that you don't want to know about" => 2, "minifridge contents, just don't ask" => 9, "Harold knows what this one is" => 32}
   lowest_key = nil
   lowest_value = nil
   harolds_things.each do |thing, num|
