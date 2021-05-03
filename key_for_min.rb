45c25e2699732e9e31c207c1a58a2982650ae390
Khaled S Momani <khaledmomani4@gmail.com>
2021-05-03 05:17:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  haroldsThings = { "sex dungeon chairs" = 5, "weird bench that you don't want to know about" = 2, "minifridge contents, just don't ask" = 9, "Harold knows what this one is" = 32}
+  
+  
 
 end
